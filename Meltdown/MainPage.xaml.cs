e99e52b38156002ca247347fc1c670391b7fa38c
Riccardo Moschetti <riccardo.moschetti@avanade.com>
2022-06-11 21:02:35 +0200

    first commit

---
+++ Meltdown/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Meltdown;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Meltdown/Platforms/Android/MainActivity.cs
@@ -0,0 +1,10 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.OS;
+
+namespace Meltdown;
+
+[Activity(Theme = "@style/Maui.SplashTheme", ScreenOrientation=ScreenOrientation.Landscape, MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+public class MainActivity : MauiAppCompatActivity
+{
+}
